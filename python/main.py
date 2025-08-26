96590c04d1bd1b2ae53c808035c159a1ecaae6ec
João Pedro <jpleite948@gmail.com>
2025-08-25 21:33:40 -0300

    Delete python directory

--- python/main.py
+++
@@ -1,9 +0,0 @@
-
-name = str(input("Digite seu nome : "))
-age = int(input("Digite sua idade : "))
-height = float(input("Digite sua altura : "))
-wheight = float(input("Digite seu peso : "))
-
-
-print(f"Ola eu me chamo {name} tenho {age} anos, 
-      tenho {height} metros e eu peso {wheight}  ")
